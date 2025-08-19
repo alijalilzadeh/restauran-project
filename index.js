c062b876b3084e1a463b074f8f260f6ff6467e7d
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-08-19 23:23:50 +0400

    Feat : Css codes were upgraded and hamburger menu added

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const navbarList = document.querySelector(".navbar-list");
 const aLinks = document.querySelectorAll(".navbar-link a");
 const container = document.querySelector(".container");
 const hElements = document.querySelectorAll(".section-title");
+const navbarLinks = document.querySelector(".navbar-links");
+const hamburgerIcon = document.querySelector(".fa-bars");
 let isDark = true;
+let isOpen = false;
+hamburgerIcon.addEventListener("click", () => {
+  navbarLinks.classList.toggle("open");
+  const isOpenCheck = isOpen ? "none" : "flex";
+  navbarLinks.style.display = isOpenCheck;
+  isOpen = !isOpen;
+    if (isOpen) {
+   
+    hamburgerIcon.className = "fa-solid fa-xmark";
+  } else {
+    hamburgerIcon.className = "fa-solid fa-bars";
+      
+  }
+})
 moonIcon.addEventListener("click", () => {
   imageBoxes.forEach(box => {
     box.classList.toggle("night-effect");
@@ -17,16 +33,19 @@ moonIcon.addEventListener("click", () => {
   })
   aLinks.forEach(aLink => {
     aLink.classList.toggle("night-effect");
+    
 
   })
-    hElements.forEach(hElement => {
+  hElements.forEach(hElement => {
     hElement.classList.toggle("night-effect");
   })
   container.classList.toggle("night-effect");
   navbarList.classList.toggle("night-effect");
+  hamburgerIcon.classList.toggle("night-effect");
   const changingTema = isDark ? "ri-sun-line" : "fa-solid fa-moon";
   moonIcon.className = changingTema;
   isDark = !isDark;
+
 });
 
 
