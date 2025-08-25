902027653cf07edd2bda659f9afd04a3ea50d2da
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-08-25 23:49:53 +0400

    Add open/close status with time-based indicator

--- index.js
+++ index.js
@@ -21,4 +21,34 @@ copyIcon1.addEventListener("click", () => {
 copyIcon2.addEventListener("click", () => {
   const textLocation = locationDetail.innerText;
   showCopyInfo(textLocation);
+
 });
+function checkStatus(){
+  const dayLists = {
+  1 : "Bazar ertəsi",
+  2 : "Çərşənbə axşamı",
+  3 : "Çərşənbə",
+  4 : "Cümə axşamı",
+  5 : "Cümə",
+  6 : "Şənbə",
+  7 : "Bazar"
+}
+  const textStatus = document.querySelector(".status p");
+  const statusCircle = document.querySelector(".circle");
+  const dayStatus = document.querySelector(".day-status");
+  const now = new Date();
+  const hour = now.getHours();
+  const day = now.getDay();
+  dayStatus.innerText = `${dayLists[day]} / 9:00 : 00:00`
+  if(hour >= 9 && hour < 24){
+    statusCircle.className ="circle open";
+    textStatus.innerText = "Açıqdır";
+  }
+  else{
+    statusCircle.className ="circle close";
+    textStatus.innerText = "Bağlıdır";
+  }
+}
+
+checkStatus();
+setInterval(checkStatus, 60000);
