b841241be3f487e8ebe410d70be253592d03c996
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-08-26 13:53:57 +0400

    Feat: In all sections gap changed to 2rem

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const copyInfo = document.querySelector(".copy-info");
 
 function showCopyInfo(text) {
   navigator.clipboard.writeText(text);
-  copyInfo.style.display = "flex"; 
-  clearTimeout(copyInfo.timeoutId); 
+  copyInfo.style.display = "flex";
+  clearTimeout(copyInfo.timeoutId);
   copyInfo.timeoutId = setTimeout(() => {
     copyInfo.style.display = "none";
   }, 1700);
@@ -23,16 +23,16 @@ copyIcon2.addEventListener("click", () => {
   showCopyInfo(textLocation);
 
 });
-function checkStatus(){
+function checkStatus() {
   const dayLists = {
-  1 : "Bazar ertəsi",
-  2 : "Çərşənbə axşamı",
-  3 : "Çərşənbə",
-  4 : "Cümə axşamı",
-  5 : "Cümə",
-  6 : "Şənbə",
-  7 : "Bazar"
-}
+    1: "Bazar ertəsi",
+    2: "Çərşənbə axşamı",
+    3: "Çərşənbə",
+    4: "Cümə axşamı",
+    5: "Cümə",
+    6: "Şənbə",
+    7: "Bazar"
+  }
   const textStatus = document.querySelector(".status p");
   const statusCircle = document.querySelector(".circle");
   const dayStatus = document.querySelector(".day-status");
@@ -40,12 +40,12 @@ function checkStatus(){
   const hour = now.getHours();
   const day = now.getDay();
   dayStatus.innerText = `${dayLists[day]} / 9:00 : 00:00`
-  if(hour >= 9 && hour < 24){
-    statusCircle.className ="circle open";
+  if (hour >= 9 && hour < 24) {
+    statusCircle.className = "circle open";
     textStatus.innerText = "Açıqdır";
   }
-  else{
-    statusCircle.className ="circle close";
+  else {
+    statusCircle.className = "circle close";
     textStatus.innerText = "Bağlıdır";
   }
 }
