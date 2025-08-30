76e565faa20a43478ef4176e50e12721d199f5d1
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-08-30 15:33:44 +0400

    Feat:Basket card added to project

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ const downPart = document.querySelector(".down-part");
 const arrowLeft = document.querySelector(".fa-arrow-left");
 const arrowUp = document.querySelector(".arrow-up");
 const addClick = document.querySelectorAll(".fa-plus");
+const basketIcon = document.querySelector(".right-side");
+const basketSideBar = document.querySelector(".basket-sidebar");
+const xMark = document.querySelector(".fa-xmark");
 let isDark = true;
 
 window.addEventListener("scroll", () => {
@@ -50,10 +53,11 @@ moonIcon.addEventListener("click", () => {
   moonIcon.className = changingTema;
   isDark = !isDark;
 });
+
 let counter = 0;
 let priceBasket = 0;
 addClick.forEach(click => {
-   
+
   click.addEventListener("click", () => {
     counter++;
     const audioClick = document.querySelector(".clickaudio");
@@ -61,18 +65,66 @@ addClick.forEach(click => {
     const counterPart = document.querySelector(".counter");
     const basketPart = document.querySelector(".left-side span");
 
+    const basketSubTotal = document.querySelector(".subtotal-price");
+    const basketTotal = document.querySelector(".total-price");
+
     const parentBox = click.parentElement;
     const foodPriceEl = parentBox.querySelector(".food-price");
     const aznCur = document.querySelector(".food-price span").textContent;
-    if (foodPriceEl) {
-      const foodPriceText = foodPriceEl.textContent;
-      const numericPrice = parseFloat(foodPriceText.replace("AZN", "").replace(",", "."));
 
-      priceBasket += numericPrice;
+    const numericPrice = calculatePrice(foodPriceEl);
 
-      counterPart.innerText = counter;
-      basketPart.innerText = priceBasket + " " + aznCur;
-    }
+    priceBasket += numericPrice;
+
+    counterPart.innerText = counter;
+    basketPart.innerText = priceBasket + " " + aznCur;
+
+    basketSubTotal.innerText =  priceBasket + " " + aznCur;
+    basketTotal.innerText = priceBasket + " " + aznCur;
   });
 });
+function calculatePrice(element) {
+  let foodPriceText = element.textContent;
+  let numeric = parseFloat(foodPriceText.replace("AZN", "").replace(",", "."));
+  
+  return numeric;
+}
 
+
+basketIcon.addEventListener("click", () => {
+  basketSideBar.classList.remove("closed");
+  basketSideBar.classList.add("opened");
+})
+
+xMark.addEventListener("click", () => {
+  basketSideBar.classList.remove("opened");
+  basketSideBar.classList.add("closed");
+})
+
+  /* 
+ <div class="basket-sidebar">
+    <div class="basket-box">
+      <div class="basket-box-upper">
+        <h2 class="basket-title">Səbətiniz</h2>
+        <i class="fa-solid fa-xmark"></i>
+      </div>
+      <div class="empty-box">
+        <i class="fa-solid fa-basket-shopping"></i>
+        <h3 class="basket-empty">Your basket is empty</h3>
+      </div>
+      <div class="orders-box">
+
+      </div>
+      <div class="total-basket-part">
+        <div class="subtotal-content">
+            <p class="subtotal-p">Subtotal</p>
+            <p class="subtotal-price">0.00 <span>&#8380;</span></p>
+        </div>
+        <div class="total-content">
+            <p class="total-p">Total</p>
+            <p class="total-price">0.00 <span>&#8380;</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+ */
